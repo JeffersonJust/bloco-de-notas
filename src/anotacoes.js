4abd83520eb1db2a0cdd63a099b9ca21708d15c7
jefferson-justino <119547340+JeffersonJust@users.noreply.github.com>
2023-06-21 18:20:26 -0300

    mudanças gerais

--- src/anotacoes.js
+++ src/anotacoes.js
@@ -1,12 +1,9 @@
 import { View, Text,  StyleSheet, ScrollView, Modal, Settings } from "react-native";
 import { Appbar, Button, Searchbar, FAB, IconButton,TextInput } from "react-native-paper";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 import { useNavigation } from "@react-navigation/native";
 
-<<<<<<< Updated upstream
-export default function Anotacoes(){
-=======
 const urlDev = "http://localhost:3000"
 const urlProduct = "https://api-backend-bd-tarde.onrender.com"
 
@@ -47,7 +44,6 @@ const[tit, setTit] = useState("")
     
 
 
->>>>>>> Stashed changes
     return(
     <View style={{backgroundColor:'#030303',flex:1}}>
 <Appbar.Header style={{backgroundColor:'#000000',
@@ -55,27 +51,17 @@ const[tit, setTit] = useState("")
             <TextInput placeholder="Título" selectionColor="white" activeOutlineColor="#4D4B4B"
             textColor="white" mode="outlined" 
              style={{backgroundColor:'#000000',fontSize:22,width:165,paddingLeft:5,
-<<<<<<< Updated upstream
-      }}  />
-      <View style={{flex:1, paddingLeft:80, paddingRight:20 }} >
-<Button mode="Contained" textColor="white" buttonColor="#1B6FEE">SALVAR</Button>
-=======
       }} 
         onChangeText={ (text ) => setTit(text)}/>
       <View style={{flex:1, paddingLeft:80, paddingRight:20 }} >
 <Button mode="Contained" textColor="white" buttonColor="#1B6FEE" 
  onPress={() => load()} >SALVAR</Button>
->>>>>>> Stashed changes
 </View>
 </Appbar.Header>
 <TextInput mode="outlined" textColor="white" multiline={true} numberOfLines={10000}
 activeOutlineColor="#4D4B4B"
-<<<<<<< Updated upstream
-style={{backgroundColor:'#030303'}} />
-=======
 style={{backgroundColor:'#030303'}}  
 onChangeText={ (text) => setDesc(text) }/>
->>>>>>> Stashed changes
             
         </View>
     )
--- src/inicial.js
+++ src/inicial.js
@@ -1,94 +1,107 @@
-import { View, Text, TextInput, StyleSheet, ScrollView, Modal, Settings } from "react-native";
-import { Appbar, Button, Searchbar, FAB, IconButton } from "react-native-paper";
-import { useState } from "react";
+import { View, Text, TextInput, StyleSheet, ScrollView, Modal, Settings, FlatList } from "react-native";
+import { Appbar, Button, Searchbar, FAB, IconButton,Divider,ActivityIndicator } from "react-native-paper";
+import { useState, useEffect,Map } from "react";
 
 import { useNavigation } from "@react-navigation/native";
 
-<<<<<<< Updated upstream
-
-=======
 const urlDev = "http://localhost:3000"
 const urlProduct = "https://api-backend-bd-tarde.onrender.com"
->>>>>>> Stashed changes
 
 export default function TelaInicial() {
   const navigator = useNavigation();
   const [modalVisible, setModalVisible] = useState(false);
-  return (
+  const [lista, setLista] = useState([])
+  const [pegaId, setPegaId] = useState()
 
-<<<<<<< Updated upstream
-    <View style={{ backgroundColor: "#000000", flex: 1 }}>
-=======
   useEffect(() => {
     const load = async () => {
       const result = await fetch(`${urlProduct}/anotacao`)
       const resultAnot = await result.json()
->>>>>>> Stashed changes
 
-      <Modal
-        transparent={true}
-        animationType="slide"
-        visible={modalVisible}>
+      console.log('resultAnot', resultAnot)
 
-        <View style={{
-          flex: 1,
-          justifyContent: 'center',
-          paddingTop: 400
-        }} >
+      setLista(resultAnot)
+    }
 
+    load()
+
+
+  }, [])
+
+  
+
+    
+  if(lista.length >0){
+    return (
+
+      <View style={{ backgroundColor: "#000000", flex: 1 }}>
+  
+        <Modal
+          transparent={true}
+          animationType="slide"
+          visible={modalVisible}>
+  
           <View style={{
             flex: 1,
-            backgroundColor: '#1573DD',
-            borderTopEndRadius: 20,
-            borderTopStartRadius: 20,
-            padding: 35,
-
-          }}>
-            <Text style={{ fontSize: 30, position: "absolute", left: 370, fontWeight: 'bold', color: 'white' }} onPress={() => setModalVisible(false)}>x</Text>
-
-            <Text style={{ color: 'white', fontSize: 20 }}>
-              Usuário:
-            </Text>
-            <Text style={{ color: 'white', fontSize: 20 }}>
-              E-mail:
-            </Text>
-            <View style={{ paddingTop: 50, gap: 15 }}>
+            justifyContent: 'center',
+            paddingTop: 400
+          }} >
+  
+            <View style={{
+              flex: 1,
+              backgroundColor: '#1573DD',
+              borderTopEndRadius: 20,
+              borderTopStartRadius: 20,
+              padding: 35,
+  
+            }}>
+              <Text style={{ fontSize: 30, position: "absolute", left: 370, fontWeight: 'bold', color: 'white' }} onPress={() => setModalVisible(false)}>x</Text>
+  
               <Text style={{ color: 'white', fontSize: 20 }}>
-                Deseja restaurar sua senha?
-
+                Usuário:
               </Text>
-              <Button style={{ borderColor: 'black' }} mode="outlined" buttonColor="#01B1FD" textColor="white"
-                onPress={() => navigator.navigate("restaurar")}
-              >RESTAURAR</Button>
+              <Text style={{ color: 'white', fontSize: 20 }}>
+                E-mail:
+              </Text>
+              <View style={{ paddingTop: 50, gap: 15 }}>
+                <Text style={{ color: 'white', fontSize: 20 }}>
+                  Deseja restaurar sua senha?
+  
+                </Text>
+                <Button style={{ borderColor: 'black' }} mode="outlined" buttonColor="#01B1FD" textColor="white"
+                  onPress={() => navigator.navigate("restaurar")}
+                >RESTAURAR</Button>
+              </View>
             </View>
           </View>
+        </Modal>
+  
+        <Appbar.Header
+          style={{
+            backgroundColor: "#0E0E0E",
+            borderBottomColor: "#4D4B4B",
+            borderBottomWidth: 2,
+          }}
+        >
+          <Appbar.Content
+            title="NOTAS"
+            titleStyle={{ color: "#FFFFFF", fontSize: 28, fontWeight: "bold" }}
+            style={{ paddingLeft: 20 }}
+          />
+  
+          <Appbar.Action icon="account-circle" size={40} color="#1573DD" onPress={() => setModalVisible(true)}
+          />
+        </Appbar.Header>
+  
+  
+  
+        <View style={{ padding: 20 }}>
+          <Searchbar placeholder="Pesquisar" iconColor="#1573DD"
+            placeholderTextColor={'#1573DD'} />
         </View>
-      </Modal>
+  
+       <View style={{paddingLeft:20,paddingRight:20,gap:20 }}>
 
-<<<<<<< Updated upstream
-      <Appbar.Header
-        style={{
-          backgroundColor: "#0E0E0E",
-          borderBottomColor: "#4D4B4B",
-          borderBottomWidth: 2,
-        }}
-      >
-        <Appbar.Content
-          title="NOTAS"
-          titleStyle={{ color: "#FFFFFF", fontSize: 28, fontWeight: "bold" }}
-          style={{ paddingLeft: 20 }}
-        />
-
-        <Appbar.Action icon="account-circle" size={40} color="#1573DD" onPress={() => setModalVisible(true)}
-        />
-      </Appbar.Header>
-
-
-
-      <View style={{ padding: 20 }}>
-        <Searchbar placeholder="Pesquisar" iconColor="#1573DD"
-          placeholderTextColor={'#1573DD'} />
-=======
         <FlatList
           data={lista}
           renderItem={({ item }) => (
@@ -112,19 +125,100 @@ export default function TelaInicial() {
         </View>
   
   
->>>>>>> Stashed changes
       </View>
+    )
+  }
+  //carregando...
+  else{
+    return(
+      <>
+       <View style={{ backgroundColor: "#000000", flex: 1 }}>
+  
+  <Modal
+    transparent={true}
+    animationType="slide"
+    visible={modalVisible}>
 
-      
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      paddingTop: 400
+    }} >
 
-<View style={{position:'absolute', margin:16,right:0,top:700}} >
-      <FAB icon={'pencil'} customSize={70}
-        style={{ backgroundColor: '#1573DD' }} onPress={() => navigator.navigate("anotar")}
-         />
+      <View style={{
+        flex: 1,
+        backgroundColor: '#1573DD',
+        borderTopEndRadius: 20,
+        borderTopStartRadius: 20,
+        padding: 35,
+
+      }}>
+        <Text style={{ fontSize: 30, position: "absolute", left: 370, fontWeight: 'bold', color: 'white' }} onPress={() => setModalVisible(false)}>x</Text>
+
+        <Text style={{ color: 'white', fontSize: 20 }}>
+          Usuário:
+        </Text>
+        <Text style={{ color: 'white', fontSize: 20 }}>
+          E-mail:
+        </Text>
+        <View style={{ paddingTop: 50, }}>
+          <Text style={{ color: 'white', fontSize: 20 }}>
+            Deseja restaurar sua senha?
+
+          </Text>
+          <Button style={{ borderColor: 'black' }} mode="outlined" buttonColor="#01B1FD" textColor="white"
+            onPress={() => navigator.navigate("restaurar")}
+          >RESTAURAR</Button>
+        </View>
+      </View>
+    </View>
+  </Modal>
+
+  <Appbar.Header
+    style={{
+      backgroundColor: "#0E0E0E",
+      borderBottomColor: "#4D4B4B",
+      borderBottomWidth: 2,
+    }}
+  >
+    <Appbar.Content
+      title="NOTAS"
+      titleStyle={{ color: "#FFFFFF", fontSize: 28, fontWeight: "bold" }}
+      style={{ paddingLeft: 20 }}
+    />
+
+    <Appbar.Action icon="account-circle" size={40} color="#1573DD" onPress={() => setModalVisible(true)}
+    />
+  </Appbar.Header>
+
+
+
+  <View style={{ padding: 20 }}>
+    <Searchbar placeholder="Pesquisar" iconColor="#1573DD"
+      placeholderTextColor={'#1573DD'} />
+  </View>
+
+ <View style={{justifyContent:'center',alignItems:'center'}}>
+
+
+    <ActivityIndicator   animating={true} color={'#1573DD'} />
+  <Text style={{color:'#1573DD'}}> AGUARDE..</Text>
+  
 </View>
 
-     
-    </View>
-  )
+  {console.log('lista:', lista)}
+  <View style={{ position: 'absolute', margin: 16, right: 0, top: 700 }} >
+    <FAB icon={'pencil'} customSize={70}
+      style={{ backgroundColor: '#1573DD' }} onPress={() => navigator.navigate("anotar")}
+    />
+  </View>
+
+
+</View>
+
+      </>
+    )
+  }
+
 }
 
